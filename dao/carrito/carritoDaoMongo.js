da8d732cdc1dc5ee47bdaea962094fe229be81ae
suecollarte <suecollarte@gmail.com>
2022-10-03 16:30:50 -0300

    Add files via upload

--- dao/carrito/carritoDaoMongo.js
+++ dao/carrito/carritoDaoMongo.js
@@ -1,1 +1,16 @@
+const ContenedorProductoMongo = require('../../contenedores/ContenedorMongodb.js');
 
+class CarritoDaoMongo extends ContenedorProductoMongo {
+
+    constructor(){
+        super('carritos',{
+            productos:{type:[], required:true}
+        })
+    }
+
+    async save(carrito = {productos:[]}){
+        return super.save(carrito)
+    }
+}
+
+module.exports= CarritoDaoMongo;
