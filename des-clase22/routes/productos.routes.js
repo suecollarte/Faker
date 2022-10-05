0e4b3a2bd5a0ec2583acf39f58d1c2890f5dbcdd
suecollarte <suecollarte@gmail.com>
2022-10-04 22:33:49 -0300

    Add files via upload

--- des-clase22/routes/productos.routes.js
+++ des-clase22/routes/productos.routes.js
@@ -1,1 +1,55 @@
+const express = require('express');
+const {Router}= express;
+const routerProducto = Router();
+const ContenedorProducto = require("../models/ContenedorProducto.js");
+const productoApi= new ContenedorProducto();
+productoApi.archivo='../DB/ProductoBD.json';
+
+
+/* Administracion */
+const config={
+  isAdmin: true
+
+}
+const esAdmin= config.isAdmin;
+function administrador(req, res,next){
+  if(!esAdmin){
+res.status(403).json({code: 403, msg:`No tiene permiso ${req.baseUrl}${req.url}`});
+
+  }
+  else {
+    next();
+  }
+
+}
+
+//Servicios
+//Router
+routerProducto.get("/", async (req, res) => {
+  console.log(productoApi.archivo);
+  const productos = await productoApi.getAll();
+  console.log("hola");
+  //res.json(productos);
+  res.json("");
+});
+
+routerProducto.get("/:id", async (req, res) => {
+  res.json(await productoApi.getById(req.params.id))
+});
+routerProducto.post("/", administrador, async(req, res) => {
+  res.json(await productoApi.save(req.params.id))  
+});
+
+routerProducto.put("/:id", administrador, async(req, res) => {
+  res.json(await productoApi.save(req.params.id))  
+});
+
+
+routerProducto.delete("/:id", administrador, async(req, res) => {
+  res.json(await productoApi.deletebyId(req.params.id))  
+});
+
+module.exports = routerProducto;
+
+
 
