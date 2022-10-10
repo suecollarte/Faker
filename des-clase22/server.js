28da12f701da24f5c145b6519eb3ea016f47032d
suecollarte <suecollarte@gmail.com>
2022-10-10 19:16:32 -0300

    Add files via upload

--- des-clase22/server.js
+++ des-clase22/server.js
@@ -1,8 +1,9 @@
-import  {express} from 'express';
-import {exphbs} from 'express-handlebars';
-const path = require('path');
-const { Server: HttpServer } = require('http');
-const { Server: IOServer } = require('socket.io');
+import  express from 'express';
+import exphbs from 'express-handlebars';
+import path from'path';
+import HttpServer  from 'http';
+//import  IOServer  from 'socket.io';
+import * as io from 'socket.io';
 
 
 /*** normalize */
@@ -25,15 +26,12 @@ console.log('Longitud objeto original: ', JSON.stringify(producto).length)
 console.log('Longitud objeto normalizado: ', JSON.stringify(normalizedProducto).length)
 
 console.log(' ------------- OBJETO DESNORMALIZADO --------------- ')
-const denormalizedProducto = denormalize(normalizedProducto.result, agrupacion, normalizedEmpresa.entities);
+const denormalizedProducto = denormalize(normalizedProducto.result, agrupacion, normalizedProducto.entities);
 print(denormalizedProducto)
 console.log('Longitud objeto original: ', JSON.stringify(producto).length)
 console.log('Longitud objeto desnormalizado: ', JSON.stringify(denormalizedProducto).length)
 
-const schemaUsuario = new schema.Entity('mensaje',{},{idAttribute:'email'});
 
-const schemaMensaje = new schema.Entity('post',{schemaUsuario},{idAttribute:'id'});
-const normalizaMensajes=(mensajesConId=>normalize(mensajesConId, schemaMensaje));
 /* --- Puerto ---*/
 const PORT = 3300;
 
@@ -54,6 +52,8 @@ app.use(express.static('./public'));
 const routerProducto  = require( "./routes/productos.routes.js");
 
 app.use("api/productos", routerProducto);
+
+
 /* --- Handlebars ---*/
 app.engine(
   'hbs',
@@ -73,7 +73,7 @@ app.get('/', (req, res) => {
 });
 
 /* --- WebSocket ---*/
-const io = new IOServer(httpServer);
+//const io = new IOServer(httpServer);
 app.set('socketio', io);
 
 io.on('connection', (socket) => {
@@ -88,13 +88,57 @@ io.on('connection', (socket) => {
     io.sockets.emit('from-server-mensajes', { DB_MENSAJE });
   });
 
+  const formAgregarProducto = document.getElementById('formAgregarProducto');
+  formAgregarProducto.addEventListener('submit',e =>{
+    e.preventDefault()
+    const producto ={
+      nombre: formAgregarProducto[0].value,
+      precio : formAgregarProducto[1].value,
+      foto: formAgregarProducto[2].value
+    }
+    socket.emit('update',producto);
+    formAgregarProducto.reset()
+  })
+
   // Producto nuevo
   socket.on('from-client-producto', (producto) => {
     DB_PRODUCTOS.push(producto);
     io.sockets.emit('from-server-producto', { DB_PRODUCTOS });
   });
+
+
+  socket.emit('productos',  productosApi.getAll());
+
+socket.on('update', async producto =>{
+  await productosApi.save(producto)
+  io.sockets.emit('productos', productosApi.getAll());
+
+})
+
+//carga mensajes
+socket.emit('mensajes',  listarMensajesNormalizado());
+//actualizar
+socket.on('nuevoMensaje', async mensaje =>{
+  mensaje.fyh = new Date().toLocaleString() 
+  await mensajesApi.save(mensaje)
+  io.sockets.emit('menajes',await listarMensajesNormalizado());
+})
+
 });
 
+
+
+const schemaUsuario = new schema.Entity('mensaje',{},{idAttribute:'email'});
+// esquema mensaje
+const schemaMensaje = new schema.Entity('post',{schemaUsuario},{idAttribute:'id'});
+const schemaMensajes= new schema.Entity('posts',{mensajes:[schemaMensaje]},{idAttribute:'id'});
+const normalizaMensajes=(mensajesConId=>normalize(mensajesConId, schemaMensaje));
+
+
+async function listarMensajesNormalizado(){
+
+  const  mensajes=await mensajes
+}
 /* --- Inicia server ---*/
 const server = httpServer.listen(PORT, () => {
   console.log(`Servidor iniciado en -> http://localhost:${PORT}`);
